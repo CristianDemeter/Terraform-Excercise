0058006a14941db57c9842eecfa360f157a525f5
Cristian Demeter <cristian.demeter@accenture.com>
2023-03-30 14:42:56 +0200

    modify varibles

--- iac-foundation/iac/shared.tf
+++ iac-foundation/iac/shared.tf
@@ -12,6 +12,10 @@ data "aws_vpc" "selected" {
 
 # AWS Info
 
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
+  
+}
 
-data "aws_region" "current" {}
+data "aws_region" "current" {
+
+}
